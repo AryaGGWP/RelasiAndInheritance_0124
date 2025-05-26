1ab9239f751ff9471011ff5bcaff9880ef07dbd0
AryaGGWP <arya.lampung.2016@gmail.com>
2025-05-27 00:59:44 +0700

    memperbaiki beberapa typo

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -1,33 +1,38 @@
 #ifndef IBU_H
 #define IBU_H
 
+#include <vector>
+
 class ibu
 {
 public:
     string nama;
-    vector<anak *> daftar_anak;
+    vector<anak*> daftar_anak;
 
-    ibu(string pNama) : nama(pNama) 
-    {
+    ibu(string pNama) : nama(pNama) {
         cout << "Ibu \"" << nama << "\" ada\n";
     }
-    ~ibu()
-    {
+
+    ~ibu() {
         cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
-    void tambahAnak(anak *);
+
+    void tambahAnak(anak*);
     void cetakAnak();
 };
-void ibu::tambahAnak(anak *pAnak)
-{
+
+void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
-void ibu::cetakAnak()
-{
-    cout << "Daftar anak dari ibu \"" << this->nama << "\":\n";
-    for (auto &a : daftar_anak)
-    {
+
+void ibu::cetakAnak() {
+    cout << "Daftar anak dari Ibu \"" << this->nama << "\":\n";
+    /*for (auto &a : daftar_anak) { 
         cout << a->nama << "\n";
+    } */
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
     }
+    cout << endl;
 }
 #endif
